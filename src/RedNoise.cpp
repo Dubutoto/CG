4d5faf837a3d2026fc88eef40dacef1ae1b26115
Geunyeong Kim <vf20093@eduroam-85-28.nomadic.bris.ac.uk>
2023-10-17 19:28:39 +0100

    week2 complete

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -21,12 +21,11 @@ std::vector <float> interpolateSingleFloats(float from, float to, int numberOfVa
 
 std::vector <glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int numberOfValues){
     std::vector <glm::vec3> result;
-    glm::vec3 term = ((to-from)/ float (numberOfValues-1));
-    glm::vec3 initialValue = from;
+    glm::vec3 term = (to - from);
+    glm::vec3 unit = term / float(numberOfValues - 1);
     for (int i = 0; i < numberOfValues; i++){
-        result.push_back(initialValue);
-        initialValue = initialValue + term;
-
+        glm::vec3 a((float)i, (float)i, (float)i);
+        result.push_back(from + unit * a);
     }
     return result;
 
@@ -54,6 +53,28 @@ void draw(DrawingWindow &window) {
 	}
 }
 
+void drawColour(DrawingWindow &window) {
+    window.clearPixels();
+
+    glm::vec3 topLeft(255, 0, 0);        // red
+    glm::vec3 topRight(0, 0, 255);       // blue
+    glm::vec3 bottomRight(0, 255, 0);    // green
+    glm::vec3 bottomLeft(255, 255, 0);   // yellow
+
+    std::vector <glm::vec3> left = interpolateThreeElementValues(topLeft,bottomLeft,WIDTH);
+    std::vector <glm::vec3> right = interpolateThreeElementValues(topRight,bottomRight,WIDTH);
+
+    for (size_t y = 0; y < window.height; y++) {
+        std::vector <glm::vec3> col = interpolateThreeElementValues(left[y], right[y], WIDTH);
+        for (size_t x = 0; x < window.width; x++) {
+
+            uint32_t colour = (255 << 24) + (int(col[x][0]) << 16) + (int(col[x][1]) << 8) + int(col[x][2]);
+            window.setPixelColour(x, y, colour);
+        }
+    }
+}
+
+
 void handleEvent(SDL_Event event, DrawingWindow &window) {
 	if (event.type == SDL_KEYDOWN) {
 		if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
@@ -70,15 +91,27 @@ int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
 
-    std::vector<float> result;
+    //test code for interpolateSingleElementValue
+   /* std::vector<float> result;
     result = interpolateSingleFloats(2.2, 8.5, 7);
-    for (size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
+    for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
     std::cout << std::endl;
+    */
 
+    //test code for interpolateThreeElementValues
+    std::vector<glm::vec3> result;
+    glm::vec3 from(1.0, 4.0, 9.2);
+    glm::vec3 to(4.0, 1.0, 9.8);
+    result = interpolateThreeElementValues(from, to, 4);
+    for(size_t i=0; i<result.size(); i++) {
+
+        std::cout << "(" << result[i][0] << "," << result[i][1] << "," << result [i][2] << ")" << " ";
+        std::cout << std::endl;
+    }
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
-		draw(window);
+		drawColour(window);
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
